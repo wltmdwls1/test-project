782e26821fe6831b271e8312b2f560109d284de1
wltmdwls1 <wltmdwls1@naver.com>
2024-08-27 02:21:58 +0900

    first-commit

---
+++ src/main/java/kr/co/jin/HiServlet.java
@@ -0,0 +1,20 @@
+package kr.co.jin;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hi-servlet")
+public class HiServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out =  resp.getWriter();
+        out.println("헬로로로로로");
+        out.close();
+    }
+}
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello!!!!!!!!ddddd!</h2>
+</body>
+</html>
